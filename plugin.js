6ff8f50302f0e71924d54117f4850660e2d9443d
albertosouza <alberto.souza.99@gmail.com>
2015-07-01 03:46:02 -0300

    fix expires time

--- plugin.js
+++ plugin.js
@@ -2,6 +2,14 @@
 module.exports = function loadPlugin(projectPath, Plugin) {
   var plugin = new Plugin(__dirname);
 
+  // set plugin configs
+  plugin.setConfigs({
+    forms: {
+      //'login': __dirname + '/server/forms/login.json',
+      'register': __dirname + '/server/forms/register.json'
+    }
+  });
+
   // set plugin routes
   plugin.setRoutes({
     // homepage | default home page
