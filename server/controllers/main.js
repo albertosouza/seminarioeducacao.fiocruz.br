c3ab75dcfce8f16c472ff4a1756b21ab73291bd9
albertosouza <alberto.souza.99@gmail.com>
2015-09-08 11:51:22 -0300

    adicionando um redirect na home page

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -3,6 +3,9 @@
  */
 
 module.exports = {
+  index: function index(req, res) {
+    res.redirect('/event');
+  },
   redirectToSendWork: function redirectToSendWork(req, res) {
     if (req.isAuthenticated()) {
       res.redirect('/event/'+req.params.eventId+'/user/'+req.user.id+'/cfwork');
