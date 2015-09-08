3129c1366a94f5f7e84b392c0730a2894bf05659
albertosouza <alberto.souza.99@gmail.com>
2015-09-08 15:50:10 -0300

    update appName

--- config/static.js
+++ config/static.js
@@ -1,4 +1,4 @@
 module.exports = {
   favicon: process.cwd() + '/node_modules/we-theme-event-seminarioeducacao.fiocruz.br/files/public/img/favicon.ico',
-  appName: 'Seminário “Educação, Saúde e Sociedade do Futuro”'
+  appName: 'Seminários e eventos da Fiocruz”'
 };
