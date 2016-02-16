a2c8e1001544a524c68435e87adb1ab74985d4f9
albertosouza <alberto.souza.99@gmail.com>
2016-02-16 18:06:41 -0200

    change default log

--- config/static.js
+++ config/static.js
@@ -1,4 +1,5 @@
 module.exports = {
+  enableRequestLog: false, // change default log
   favicon: process.cwd() + '/node_modules/we-theme-event-seminarioeducacao.fiocruz.br/files/public/img/favicon.ico',
   appName: 'Seminários e eventos da Fiocruz”'
 };
